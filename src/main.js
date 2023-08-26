ecaf44df5c2d4977b4b98866ba9b7ba01ecb334b
tan-sd <shengdatan@gmail.com>
2023-08-26 16:57:58 +0800

    Add TroisJS

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { TroisJSVuePlugin } from "troisjs";
 
 const app = createApp(App);
-app.use(router);
+app.use(router).use(TroisJSVuePlugin);
 
 app.mount("#app");
