7bec7afa8491d3d2b402bb67dfc87c0752dbf673
tan-sd <shengdatan@gmail.com>
2023-08-26 22:18:57 +0800

    Styling and signInStatus implementation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,23 @@
     <nav class="navbar">
         <div class="container-fluid">
             <router-link to="/" class="navbar-brand">
-                <img class="logo" src="../assets/images/logo/zenbuddy-logo.png" />
+                <img
+                    class="logo"
+                    src="../assets/images/logo/zenbuddy-logo.png"
+                />
             </router-link>
             <div>
-                <ul class="navbar-nav">
+                <ul class="navbar-nav me-4">
                     <li class="nav-item">
-                        <router-link to="/Login" class="nav-item">
+                        <router-link
+                            v-if="signInStatus"
+                            to="/"
+                            class="nav-item"
+                            @click="handleSignOut"
+                        >
+                            Sign out
+                        </router-link>
+                        <router-link v-else to="/Login" class="nav-item">
                             Login
                         </router-link>
                     </li>
@@ -17,9 +28,30 @@
     </nav>
 </template>
 
+<script>
+import { auth, signOutUser } from "../firebase/index";
+export default {
+    data() {
+        return {
+            signInStatus: false,
+        };
+    },
+    methods: {
+        handleSignOut() {
+            signOutUser();
+        },
+    },
+    created() {
+        auth.onAuthStateChanged((user) => {
+            this.signInStatus = user !== null;
+        });
+    },
+};
+</script>
+
 <style>
-    .logo {
-        height: 60px;
-        width: auto;
-    }
+.logo {
+    height: 60px;
+    width: auto;
+}
 </style>
