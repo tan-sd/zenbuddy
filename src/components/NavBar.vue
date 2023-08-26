9c9f150fb3cba28b2199042eeb140d8a32ea50dc
tan-sd <shengdatan@gmail.com>
2023-08-26 12:43:42 +0800

    Initalise NavBar components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <router-link to="/" class="navbar-brand"> Logo </router-link>
+            <div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/Login" class="nav-item">
+                            Login
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
